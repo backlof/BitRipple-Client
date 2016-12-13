1c3ac6df9b49cfdd169b311b5521a18a3a54d610
backlof <backlof@github.com>
2016-12-13 21:56:10 +0100

    Cleaned up the service

---
+++ Source/BitRipple/Service/ConsoleApplication/ConsoleApplication.cs
@@ -0,0 +1,81 @@
+﻿using Models;
+using Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitRipple
+{
+	 public class ConsoleApplication : IApplicationService
+	 {
+		  public BitRippleRepository Repository { get; set; }
+
+		  public ConsoleApplication(BitRippleRepository repository)
+		  {
+				Repository = repository;
+		  }
+
+		  public void Run()
+		  {
+				var loadResult = Repository.Data.Load();
+				if (loadResult.Success)
+				{
+					 ConsoleInterval.Run(Task.Run(GoThroughFeeds), TimeSpan.FromMinutes(Repository.Data.Settings.Interval));
+				}
+				else
+				{
+					 Logger.WriteErrors(loadResult);
+					 Logger.Log("Application did not start");
+					 Console.Write("Press enter to exit: ");
+					 Console.Read();
+				}
+		  }
+
+		  private async Task GoThroughFeeds()
+		  {
+				Logger.Log("Updating...");
+
+				var updatedFeeds = await TaskEnumerator.WhenAll(FetchRss, Repository.Data.Feeds);
+
+				var torrentsToDownload = await TaskEnumerator.WhenAll(FindTorrentsToDownload, updatedFeeds);
+
+				await TaskEnumerator.WhenAll(DownloadTorrent, torrentsToDownload.SelectMany(x => x));
+
+				Repository.Data.Save();
+		  }
+
+		  public Feed FetchRss(Feed feed)
+		  {
+				System.Diagnostics.Debug.WriteLine("Feeder");
+
+				var feedUpdateResult = Repository.Downloader.DownloadFeed(feed);
+				if (feedUpdateResult.Success)
+				{
+					 feed.Torrents = feedUpdateResult.Value;
+					 return feed;
+				}
+				else
+				{
+					 Logger.WriteErrors(feedUpdateResult);
+					 return feed;
+				}
+		  }
+
+		  public void DownloadTorrent(Download torrent)
+		  {
+				Logger.Log($"Downloading: <{torrent.Name}>");
+
+				var downloadResult = Repository.Downloader.DownloadTorrent(torrent);
+				if (!downloadResult.Success)
+				{
+					 Logger.WriteErrors(downloadResult);
+				}
+		  }
+
+		  public IEnumerable<Download> FindTorrentsToDownload(Feed feed)
+		  {
+				return Repository.Filter.FindTorrentsMatchingFilters(feed);
+		  }
+	 }
+}
---
+++ Source/BitRipple/Service/ConsoleApplication/ConsoleInterval.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BitRipple
+{
+	 public class ConsoleInterval
+	 {
+		  public static void Run(Task action, TimeSpan ts)
+		  {
+				InternalRun(action, ts, new CancellationTokenSource());
+		  }
+
+		  public static void Run(Action action, TimeSpan ts)
+		  {
+				InternalRun(Task.Run(action), ts, new CancellationTokenSource());
+		  }
+
+		  private static void InternalRun(Task action, TimeSpan ts, CancellationTokenSource tks)
+		  {
+				IntervalRunner(action, ts, tks);
+				Console.Read();
+				tks.Cancel();
+		  }
+
+		  private static async void IntervalRunner(Task action, TimeSpan ts, CancellationTokenSource tks)
+		  {
+				try
+				{
+					 do
+					 {
+						  action.Wait();
+						  await Task.Delay(ts, tks.Token);
+					 } while (tks.IsCancellationRequested);
+				}
+				catch (TaskCanceledException)
+				{
+					 Environment.Exit(0);
+				}
+		  }
+	 }
+}
